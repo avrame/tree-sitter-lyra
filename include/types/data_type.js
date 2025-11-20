f8e78f823e019bae71c97639de0d80ec5a4c2e7c
Avram Eisner <avrame@gmail.com>
2025-11-19 21:35:22 -0800

    Refactored data type constructor definitions to unify syntax and improve grammar structure. Updated related tests to reflect changes in data type constructor handling, including support for generic parameters and structured bodies.

--- include/types/data_type.js
+++ include/types/data_type.js
@@ -5,19 +5,22 @@ module.exports = {
     optional($.generic_parameters),
     '=',
     optional("|"), // Optional pipe for nice formatting (one constructor per line below data type name)
-    $._data_type_constructor,
-    repeat(seq('|', $._data_type_constructor))
+    $.data_type_constructor,
+    repeat(seq('|', $.data_type_constructor))
   ),
 
-  _data_type_constructor: $ => prec.right(seq(
-    $.data_type_constructor_name,
-    optional(
-      choice(
-        $.type,
-        $.struct_type_body,
-      )
-    )
-  )),
-
+  data_type_constructor: $ => choice(
+    seq(
+      field('name', $.data_type_constructor_name),
+      '(',
+      field('param', $.type),
+      ')'
+    ),
+    seq(
+      field('name', $.data_type_constructor_name),
+      field('body', $.struct_type_body),
+    ),
+    field('name', $.data_type_constructor_name)
+  ),
   data_type_constructor_name: $ => /[A-Z][a-zA-Z0-9]*/,
 }

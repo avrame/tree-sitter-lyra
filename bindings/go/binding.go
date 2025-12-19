bd63619082e7cded2024745b4559a334a6f07e59
Avram Eisner <avrame@gmail.com>
2025-12-18 23:04:45 -0800

    Update module path and add indirect dependency: Changed module name from 'github.com/tree-sitter/tree-sitter-lyra_parser' to 'github.com/avrame/tree-sitter-lyra'. Added indirect dependency on 'github.com/mattn/go-pointer v0.0.1' and updated related test imports accordingly.

--- bindings/go/binding.go
+++ bindings/go/binding.go
@@ -11,5 +11,5 @@ import "unsafe"
 
 // Get the tree-sitter Language for this grammar.
 func Language() unsafe.Pointer {
-	return unsafe.Pointer(C.tree_sitter_lyra_parser())
+	return unsafe.Pointer(C.tree_sitter_lyra())
 }
--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,12 +3,12 @@ package tree_sitter_lyra_parser_test
 import (
 	"testing"
 
+	lyra_parser "github.com/avrame/tree-sitter-lyra/bindings/go"
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
-	tree_sitter_lyra_parser "github.com/tree-sitter/tree-sitter-lyra_parser/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
-	language := tree_sitter.NewLanguage(tree_sitter_lyra_parser.Language())
+	language := tree_sitter.NewLanguage(lyra_parser.Language())
 	if language == nil {
 		t.Errorf("Error loading The Lyra Programming Language Parser grammar")
 	}
